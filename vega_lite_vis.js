37473d4cc8ce07da2122f5d5bbe1ac63b32e485e
abaj0005 <abaj0005@student.monash.edu>
2022-10-13 02:35:10 +1100

    Initial Commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,24 @@
+var vg_1 = "visualization_usa.vg.json";
+vegaEmbed("#vg-1", vg_1).then(function (result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_2 = "visualization_bar.vg.json";
+vegaEmbed("#vg-2", vg_2).then(function (result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "visualization_interactive.vg.json";
+vegaEmbed("#vg-3", vg_3).then(function (result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "visualization_florida.json";
+vegaEmbed("#vg-4", vg_4).then(function (result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
