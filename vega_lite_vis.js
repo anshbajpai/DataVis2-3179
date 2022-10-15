0add45a2adb42d0c2ca5620bb7292b697455d9b2
abaj0005 <abaj0005@student.monash.edu>
2022-10-16 00:57:04 +1100

    Updated design layout, with updated format and colors with viz

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,24 +1,36 @@
+
+var opt = {
+    actions: false,
+    defaultStyle: false
+}
+
 var vg_1 = "visualization_usa.vg.json";
-vegaEmbed("#vg-1", vg_1).then(function (result) {
+vegaEmbed("#vg-1", vg_1, opt).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
-var vg_2 = "visualization_bar.vg.json";
-vegaEmbed("#vg-2", vg_2).then(function (result) {
+var vg_2 = "visualization_bar_int.vg.json";
+vegaEmbed("#vg-2", vg_2, opt).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "visualization_interactive.vg.json";
-vegaEmbed("#vg-3", vg_3).then(function (result) {
+var vg_3 = "visualization_bar_states_int.vg.json";
+vegaEmbed("#vg-3", vg_3, opt).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "visualization_florida.json";
 vegaEmbed("#vg-4", vg_4).then(function (result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "visualization_bar_trump.vg.json";
+vegaEmbed("#vg-5", vg_5, opt).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
